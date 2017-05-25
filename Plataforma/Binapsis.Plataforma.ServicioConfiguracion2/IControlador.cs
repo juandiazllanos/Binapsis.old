f3c6a98639e667439a0b39fbd8346aa7cb275f96
JUAN <juandll81@hotmail.com>
2017-05-25 17:30:28 -0500

    25.05.2017

--- Plataforma/Binapsis.Plataforma.ServicioConfiguracion2/IControlador.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Binapsis.Plataforma.ServicioConfiguracion
-{
-    internal interface IControlador
-    {
-        void Establecer(string valor);
-        string Obtener(string consulta);
-        string CadenaConexion { get; set; }
-    }
-}
